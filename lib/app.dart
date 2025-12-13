f2cf6f265acbef550ead7bcf69ce5f6e351a0530
zoh01 <adebayozoh@gmail.com>
2025-12-13 21:22:23 +0100

    App

--- lib/app.dart
+++ lib/app.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/get_navigation.dart';
-import 'package:interior_design/features/authentication/screens/onboarding/onboarding.dart';
 import 'package:interior_design/features/authentication/screens/splash_screen/splash_screen.dart';
-import 'package:interior_design/utils/constants/colors.dart';
 import 'package:interior_design/utils/theme/theme.dart';
 
 import 'bindings/general_bindings.dart';
